a2469a2e713790f475c56eef9c639826a8f97ef5
Lisa Wand <lisavenneker21@gmail.com>
2020-10-18 17:09:38 -0400

    api thecocktaildb and api chcuk norris work

--- java.js
+++ java.js
@@ -1,39 +1,17 @@
+console.log("connect check")
 //API info cocktails
 const API_KEY = "1"
+//const drink = drink // this will be used in form, for user to enter a drink search, currently defaul is at margarita
 const API_URL = `https://www.thecocktaildb.com/api/json/v1/${API_KEY}/search.php?s=margarita`
 
 //API info Chuck Norris
+//API chuck will give random quotes - it will be something like response.data.categories.value
 const API_URL_CHUCK = "https://api.chucknorris.io/jokes/random"
 
 
-//check API cocktails
-
-const getCocktail = () => {
-    axios.get(API_URL).then(getCocktail);
-}
-console.log(getCocktail())
-
-// const getCocktail = async () => {
-//     try {
-//         const response = await axios.get(API_URL)
-//         console.log(response)
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
-
-document.querySelector(".cocktaill").addEventListener("click", getCocktail);
-
-////////
-
-const getQuote = () => {
-    axios.get(API_URL_CHUCK).then(showQuote);
-    console.log(axios)
-}
-console.log(showQuote())
-
-
+//API check Chuck Norris
 const getQuote = async () => {
+    console.log("first")
     try {
         const response = await axios.get(API_URL_CHUCK)
         console.log(response)
@@ -43,4 +21,23 @@ const getQuote = async () => {
 }
 console.log(getQuote)
 
+
+
+//check API cocktails
+const getCocktail = async () => {
+    console.log("first")
+    try {
+        const response = await axios.get(API_URL)
+        console.log(response)
+    } catch (error) {
+        console.log(error)
+    }
+}
+console.log(getCocktail)
+
+//API cocktail
+document.querySelector(".cocktail").addEventListener("click", getCocktail);
+
+//API Chuck
 document.querySelector(".chuck").addEventListener("click", getQuote)
+
