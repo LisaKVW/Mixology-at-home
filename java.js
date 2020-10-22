3563f27b6770bef2ef83d49cfe5ca9277b7a8318
Lisa Wand <lisavenneker21@gmail.com>
2020-10-22 00:49:57 -0400

    CSS updated - assigned borders and classes to easily allocate elements and style

--- java.js
+++ java.js
@@ -48,77 +48,77 @@ const showSearchCocktail = async (inputSearch) => {
     console.log(image)
     let instructions = drink.strInstructions
     console.log(instructions)
-    let showRecipe = document.querySelector("#showRecipe")
-    showRecipe.innerHTML = `
+    let showCocktail = document.querySelector("#showCocktail")
+    showCocktail.innerHTML = `
         <div class="showCocktail"> 
-        <h2 id="cocktailName"> ${selectedDrinkName} </h2>
-        <img src=${image} id="cocktailImage" /> </div>
-        <h3> Ingredients </h3> 
-        <span id="measurement">${drink.strMeasure1}  </span>
-        <span id="ingredient">${drink.strIngredient1} , </span>`
+        <h2  class="showCocktail" id="cocktailName"> ${selectedDrinkName} </h2>
+        <img class="showCocktail" src=${image} id="cocktailImage" /> 
+        <h3 class="showCocktail"> Ingredients </h3> 
+        <span class="showCocktail" id="measurement">${drink.strMeasure1}  </span>
+        <span class="showCocktail" id="ingredient">${drink.strIngredient1} , </span> </div>`
 
     if (drink.strIngredient2 !== null) {
-      showRecipe.innerHTML +=
-        ` <span id="measurement">${drink.strMeasure2} </span>
-        <span id="ingredient">${drink.strIngredient2} , </span>`
+      showCocktail.innerHTML +=
+        ` <span class="showCocktail" id="measurement">${drink.strMeasure2} </span>
+        <span class="showCocktail" id="ingredient">${drink.strIngredient2} , </span> </div>`
     }
 
     if (drink.strIngredient3 !== null) {
-      showRecipe.innerHTML +=
-        ` <span id="measurement">${drink.strMeasure3} </span>
-        <span id="ingredient">${drink.strIngredient3} , </span> `
+      showCocktail.innerHTML +=
+        ` <span class="showCocktail" id="measurement">${drink.strMeasure3} </span>
+        <span class="showCocktail" id="ingredient">${drink.strIngredient3} , </span> `
     }
 
     if (drink.strIngredient4 !== null) {
-      showRecipe.innerHTML +=
-        `<span id="measurement">${drink.strMeasure4} </span>
-        <span id="ingredient">${drink.strIngredient4} , </span> `
+      showCocktail.innerHTML +=
+        `<span class="showCocktail" id="measurement">${drink.strMeasure4} </span>
+        <span class="showCocktail" id="ingredient">${drink.strIngredient4} , </span> `
     }
 
     if (drink.strIngredient5 !== null) {
-      showRecipe.innerHTML +=
-        `<span id="measurement">${drink.strMeasure5} </span>
-        <span id="ingredient">${drink.strIngredient5} , </span> `
+      showCocktail.innerHTML +=
+        `<span class="showCocktail" id="measurement">${drink.strMeasure5} </span>
+        <span class="showCocktail" id="ingredient">${drink.strIngredient5} , </span> `
     }
 
     if (drink.strIngredient6 !== null) {
-      showRecipe.innerHTML +=
-        ` <span id="measurement">${drink.strMeasure6} </span>
-        <span id="ingredient">${drink.strIngredient6} , </span> `
+      showCocktail.innerHTML +=
+        ` <span class="showCocktail" id="measurement">${drink.strMeasure6} </span>
+        <span class="showCocktail" id="ingredient">${drink.strIngredient6} , </span> `
     }
 
     if (drink.strIngredient7 !== null) {
-      showRecipe.innerHTML +=
-        `<span id="measurement">${drink.strMeasure7} </span>
-        <span id="ingredient">${drink.strIngredient7} , </span>`
+      showCocktail.innerHTML +=
+        `<span class="showCocktail" id="measurement">${drink.strMeasure7} </span>
+        <span class="showCocktail" id="ingredient">${drink.strIngredient7} , </span>`
     }
 
     if (drink.strIngredient8 !== null) {
-      showRecipe.innerHTML +=
-        `<span id="measurement">${drink.strMeasure8} </span>
-        <span id="ingredient">${drink.strIngredient8} , </span>`
+      showCocktail.innerHTML +=
+        `<span class="showCocktail" id="measurement">${drink.strMeasure8} </span>
+        <span class="showCocktail" id="ingredient">${drink.strIngredient8} , </span>`
     }
 
     if (drink.strIngredient9 !== null) {
-      showRecipe.innerHTML +=
-        `<span id="measurement">${drink.strMeasure9} </span>
-        <span id="ingredient">${drink.strIngredient9} , </span>`
+      showCocktail.innerHTML +=
+        `<span class="showCocktail" id="measurement">${drink.strMeasure9} </span>
+        <span class="showCocktail" id="ingredient">${drink.strIngredient9} , </span>`
     }
 
     if (drink.strIngredient10 !== null) {
-      showRecipe.innerHTML +=
-        `<span id="measurement">${drink.strMeasure10} </span>
-        <span id="ingredient">${drink.strIngredient10} , </span>`
+      showCocktail.innerHTML +=
+        `<span class="showCocktail" id="measurement">${drink.strMeasure10} </span>
+        <span class="showCocktail" id="ingredient">${drink.strIngredient10} , </span>`
     }
 
     if (drink.strIngredient11 !== null) {
-      showRecipe.innerHTML +=
-        `<span id="measurement">${drink.strMeasure11} </span>
-        <span id="ingredient">${drink.strIngredient11} , </span>`
+      showCocktail.innerHTML +=
+        `<span class="showCocktail" id="measurement">${drink.strMeasure11} </span>
+        <span class="showCocktail" id="ingredient">${drink.strIngredient11} , </span>`
     }
-    showRecipe.innerHTML += `<h3> Instructions </h3>
-    <p id="instructions"> ${instructions}. Serve in a ${glass}</p>
-    <p> Don't forget to taste your drink first before serving! </p>`
+    showCocktail.innerHTML += `<h3 class="showCocktail"> Instructions </h3>
+    <p class="showCocktail" id="instructions"> ${instructions}. Serve in a ${glass}</p>
+    <p class="showCocktail" Don't forget to taste your drink first before serving! </p>`
 
   } catch (error) {
     console.log(error)
@@ -143,8 +143,7 @@ const getRandomDrink = async () => {
     const response = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/random.php")
     console.log(response)
     let showRandomDrink = document.querySelector("#showRandomDrink")
-    let showRandomIngMeas = document.querySelector("#showRandomIngMeas")
-    let showRandomSteps = document.querySelector("#showRandomSteps")
+
 
     let drink = response.data.drinks[0]
     console.log(drink)
@@ -156,47 +155,75 @@ const getRandomDrink = async () => {
     console.log(image)
     let instructions = drink.strInstructions
     console.log(instructions)
-    let ingredients = [
-      drink.strIngredient1,
-      drink.strIngredient2,
-      drink.strIngredient3,
-      drink.strIngredient4,
-      drink.strIngredient5,
-      drink.strIngredient6,
-      drink.strIngredient7,
-      drink.strIngredient8,
-      drink.strIngredient9,
-      drink.strIngredient10,
-      drink.strIngredient11
-    ]
 
-    let measurement = [
-      drink.strMeasure1,
-      drink.strMeasure2,
-      drink.strMeasure3,
-      drink.strMeasure4,
-      drink.strMeasure5,
-      drink.strMeasure6,
-      drink.strMeasure7,
-      drink.strMeasure8,
-      drink.strMeasure9,
-      drink.strMeasure10,
-      drink.strMeasure11
-    ]
-    for (let i = 0; i < measurement.length; i++) { // created a for loop, to loop through the ingredients and measurements
-      if (measurement[i] !== null) {  // added if - so when loop hits a null result, it stops
-        let divMeasIng = document.createElement("div") // created a new div element - this so we can interpolate our looped resutls on the html
-        divMeasIng.innerHTML = `${measurement[i]} ${ingredients[i]}`
-        showRandomIngMeas.append(divMeasIng) // append our new div at 
-      }
-    }
-    showRandomDrink.innerHTML =
-      ` <div class="showCocktail"> 
+    showRandomDrink.innerHTML = `
+        <div class="showCocktail"> 
         <h2 id="cocktailName"> ${selectedDrinkName} </h2>
-        <img src=${image} id="cocktailImage" /> </div> 
-         <h3> Ingredients </h3> `
+        <img src=${image} id="cocktailImage" /> </div>
+        <h3> Ingredients </h3> 
+        <span id="measurement">${drink.strMeasure1}  </span>
+        <span id="ingredient">${drink.strIngredient1} , </span>`
 
-    showRandomSteps.innerHTML += `<h3> Instructions </h3>
+    if (drink.strIngredient2 !== null) {
+      showRandomDrink.innerHTML +=
+        ` <span id="measurement">${drink.strMeasure2} </span>
+        <span id="ingredient">${drink.strIngredient2} , </span>`
+    }
+
+    if (drink.strIngredient3 !== null) {
+      showRandomDrink.innerHTML +=
+        ` <span id="measurement">${drink.strMeasure3} </span>
+        <span id="ingredient">${drink.strIngredient3} , </span> `
+    }
+
+    if (drink.strIngredient4 !== null) {
+      showRandomDrink.innerHTML +=
+        `<span id="measurement">${drink.strMeasure4} </span>
+        <span id="ingredient">${drink.strIngredient4} , </span> `
+    }
+
+    if (drink.strIngredient5 !== null) {
+      showRandomDrink.innerHTML +=
+        `<span id="measurement">${drink.strMeasure5} </span>
+        <span id="ingredient">${drink.strIngredient5} , </span> `
+    }
+
+    if (drink.strIngredient6 !== null) {
+      showRandomDrink.innerHTML +=
+        ` <span id="measurement">${drink.strMeasure6} </span>
+        <span id="ingredient">${drink.strIngredient6} , </span> `
+    }
+
+    if (drink.strIngredient7 !== null) {
+      showRandomDrink.innerHTML +=
+        `<span id="measurement">${drink.strMeasure7} </span>
+        <span id="ingredient">${drink.strIngredient7} , </span>`
+    }
+
+    if (drink.strIngredient8 !== null) {
+      showRandomDrink.innerHTML +=
+        `<span id="measurement">${drink.strMeasure8} </span>
+        <span id="ingredient">${drink.strIngredient8} , </span>`
+    }
+
+    if (drink.strIngredient9 !== null) {
+      showRandomDrink.innerHTML +=
+        `<span id="measurement">${drink.strMeasure9} </span>
+        <span id="ingredient">${drink.strIngredient9} , </span>`
+    }
+
+    if (drink.strIngredient10 !== null) {
+      showRandomDrink.innerHTML +=
+        `<span id="measurement">${drink.strMeasure10} </span>
+        <span id="ingredient">${drink.strIngredient10} , </span>`
+    }
+
+    if (drink.strIngredient11 !== null) {
+      showRandomDrink.innerHTML +=
+        `<span id="measurement">${drink.strMeasure11} </span>
+        <span id="ingredient">${drink.strIngredient11} , </span>`
+    }
+    showRandomDrink.innerHTML += `<h3> Instructions </h3>
     <p id="instructions"> ${instructions}. Serve in a ${glass}</p>
     <p> Don't forget to taste your drink first before serving! </p>`
 
